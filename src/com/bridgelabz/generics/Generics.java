c72fe6a0355780e025b677925639290da7c94312
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-02 23:38:02 -0700

    Added : More than three values and extends Comparable

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -3,17 +3,24 @@ package com.bridgelabz.generics;
 public class Generics {
     public static void main(String[] args) {
 
+        //String
         String a1 = "Apple",b1 = "Peach",c1 = "Banana";
-        GenericsTest test1 = new GenericsTest(a1,b1,c1);
-        test1.maximum();
+        GenericsTest <String> test1 = new GenericsTest(a1,b1,c1);
+        System.out.println(test1.findMaximum(a1,b1,c1));
 
-        String a2 = "One",b2 = "Two",c2 = "Three";
-        GenericsTest test2 = new GenericsTest(a2,b2,c2);
-        test2.maximum();
+        //Integer
+        Integer a2 = 30,b2 = 50,c2 = 40;
+        GenericsTest <Integer> test2 = new GenericsTest(a2,b2,c2);
+        System.out.println(test2.findMaximum(a2,b2,c2));
 
-        String a3 = "Red",b3 = "Blue",c3 = "Green";
-        GenericsTest test3 = new GenericsTest(a3,b3,c3);
-        test3.maximum();
+        //Float
+        Float a3 = 10.5f,b3 = 12.7f,c3 = 17.7f;
+        GenericsTest <Float> test3 = new GenericsTest(a3,b3,c3);
+        System.out.println(test3.findMaximum(a3,b3,c3));
+
+        Integer a4 = 25, b4 = 40, c4 = 65, d4 = 75;
+        GenericsTest <Integer> test4 = new GenericsTest(a4,b4,c4,d4);
+        System.out.println(test4.findMaximum(a4,b4,c4,d4));
 
     }
 }
--- src/com/bridgelabz/generics/GenericsTest.java
+++ src/com/bridgelabz/generics/GenericsTest.java
@@ -1,29 +1,39 @@
 package com.bridgelabz.generics;
 
-class GenericsTest {
+    public class GenericsTest <T extends Comparable<T>> {
 
+        private T a,b,c,d;
 
-    private static String a,b,c;
-
-    GenericsTest(String a, String b, String c){
-        this.a = a;
-        this.b = b;
-        this.c= c;
-    }
-
-    public void maximum() {
-        String res=GenericsTest.findMaximum(a,b,c);
-        System.out.println("Maximum of "+a+", "+b+" and "+c+" is: " +res);
-    }
-
-    public static String findMaximum(String a, String b, String c) {
-        String max = a;
-        if(b.compareTo(max) > 0) {
-            max = b;
+        //constructor with generic
+        GenericsTest(T a, T b, T c){
+            this.a = a;
+            this.b = b;
+            this.c= c;
         }
-        if(c.compareTo(max) > 0) {
-            max = c;
+
+        GenericsTest(T a, T b, T c, T d){
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
         }
-        return max;
+
+        public static <T extends Comparable<T>> T findMaximum(T ...values) {
+            T max = values[0];
+            for (T element : values) {
+                if (element.compareTo(max) > 0) {
+                    max = element;
+                }
+            }
+            printMax(max,values);
+            return max;
+        }
+
+        private static <T extends Comparable<T>>  void printMax(T max, T ...values) {
+            System.out.println("Maximum of ");
+            for(T t : values) {
+                System.out.print(t+" ");
+            }
+            System.out.println("is " + max);
+        }
     }
-}
